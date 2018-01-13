3687da436096dbf0bed3f4ecc630957388f596a4
yaacov <kobi.zamir@gmail.com>
2018-01-13 17:06:49 +0200

    Add tags to example script

--- examples/push_metrics_example.sh
+++ examples/push_metrics_example.sh
@@ -4,34 +4,37 @@
 # requires mohawk server running on 127.0.0.1:8080
 # e.g. mohawk -V &
 
-usage="$(basename "$0") [-h] [-u -s -d -t] -- send random data into mohawk server
+usage="$(basename "$0") [-h] [-u -s -t -d -g] -- send random data into mohawk server
 
 where:
     -h  show this help text
-    -u  server url (default: 127.0.0.1:8080)
+    -u  server url (default: http://127.0.0.1:8080)
     -t  tenant (default: _ops)
     -d  data id (default: free_memory)
+    -g  data tags (default: node:localhost,group:usage)
     -s  sleep after each data push in sec (default: 30)
 
 example:
     [ run mohawk: ./mohawk --options=granularity=1s --storage=memory ]
 
     # run push \"cpu_usage\" metrics each 1s using the \"_system\" tenant
-    ./push_metrics_example.sh -t _ops -s 1 -d cpu_usage
+    ./push_metrics_example.sh -t _ops -s 1 -d cpu_usage -g name:cpu_usage
 "
 
-MOHAWK_URL=127.0.0.1:8080
+MOHAWK_URL=http://127.0.0.1:8080
 MOHAWK_TENANT=_ops
 DATA_ID=free_memory
 SLEEP=30
+TAGS=node:localhost,group:usage
 
-while getopts u:s:d:t:h option
+while getopts u:s:d:g:t:h option
 do
  case "${option}"
  in
  u) MOHAWK_URL=${OPTARG};;
  s) SLEEP=${OPTARG};;
  d) DATA_ID=${OPTARG};;
+ g) TAGS=${OPTARG};;
  t) MOHAWK_TENANT=${OPTARG};;
  h) echo "$usage"
     exit;;
@@ -41,10 +44,19 @@ do
  esac
 done
 
+if [ -n "$TAGS" ]; then
+  TAGS=\"$(echo $TAGS | sed 's/:/\":\"/g' | sed 's/,/\",\"/g')\"
+  curl -k ${MOHAWK_URL}/hawkular/metrics/gauges/tags \
+       -X PUT \
+       -H "Content-Type: application/json" \
+       -H "Hawkular-Tenant: ${MOHAWK_TENANT}" \
+       -d "[{\"id\":\"${DATA_ID}\",\"tags\":{${TAGS}}}]"
+fi
+
 while true; do
   # Post some fake data to mohawk
   VAL=$(($RANDOM % 100))
-  curl http://${MOHAWK_URL}/hawkular/metrics/gauges/raw \
+  curl -k ${MOHAWK_URL}/hawkular/metrics/gauges/raw \
        -H "Content-Type: application/json" \
        -H "Hawkular-Tenant: ${MOHAWK_TENANT}" \
        -d "[{\"id\":\"${DATA_ID}\",\"data\":[{\"timestamp\":$(date +%s)000,\"value\":${VAL}}]}]"
