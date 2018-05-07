96157322bb4745b320c3eecd22b8181bcb049759
yaacov <kobi.zamir@gmail.com>
2018-05-07 11:59:54 +0300

    python example server lint

--- src/alerts/examples/alert-buffer.py
+++ src/alerts/examples/alert-buffer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+
 class AlertHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         # print out new alert changes
-        print self.rfile.read(int(self.headers['Content-Length']))
+        print(self.rfile.read(int(self.headers['Content-Length'])))
 
         self.send_response(200)
         self.end_headers()
@@ -12,8 +13,9 @@ class AlertHandler(BaseHTTPRequestHandler):
 
 def run():
     httpd = HTTPServer(('0.0.0.0', 9099), AlertHandler)
-    print 'Starting httpd...'
+    print('Starting httpd...')
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run()
